145291af4bde4ee1184b3676c592e01394af9517
Martin Flucka <martin.flucka@gmail.com>
2020-08-08 22:49:26 +0200

    [chore] Rename test

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pixel_fit/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Test text should be present', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
     expect(find.text('Test'), findsOneWidget);
   });
